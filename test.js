56240d73caf408929a312a887049c7e2ea2fbba9
ElktrumElk <festinacole373@gmail.com>
2026-02-11 21:45:10 +0000

    update

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-import Input from "./node_modules/interacter/ios.js"
+import { writeLine } from "./IOS.js";
+import {Input} from "./node_modules/interacter/ios.js"
 import { main } from "./node_modules/interacter/ios.js"
 
 
 main(async () => {
-    const username = new Input()
+    const username = new Input();
+    const password = new Input();
+
+    await username.readl("Enter username: ");
+    await password.readl("Enter password: ");
+
+    writeLine(username.value, password.value)
 })
